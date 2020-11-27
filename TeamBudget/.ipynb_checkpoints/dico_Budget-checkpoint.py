fa2b51f228bb80d5006f7239ea28458920e89364
CANEVETGASPARD <canevet.gaspard@gmail.com>
2020-11-27 12:44:12 +0100

    creation de dataset avec le budget des equipes

---
+++ TeamBudget/.ipynb_checkpoints/dico_Budget-checkpoint.py
@@ -0,0 +1,92 @@
+﻿#création du dicot score_FIFA contenant les score des équipes de L1 sur FIFA de 2015 à 2021
+#les budget sont en million
+
+dic_2016={"Paris SG":490 ,"Monaco":250,"Lyon":170,"Marseille":125,"St Etienne":68,
+"Rennes":43,"Lille":70,"Bordeaux":60,"Nice":40,"Toulouse":30,"Guingamp":25,
+"Montpellier":42,"Lorient":30,"Nantes":38,"Reims":31,"Caen":26,"Angers":24,
+"Bastia":25,"Troyes":23,"Ajaccio GFCO":14}
+
+
+dic_2017={"Paris SG":500,"Monaco":145,"Lyon":235,"Marseille":100,"St Etienne":70,
+"Rennes":50,"Lille":75,"Bordeaux":60,"Nice":42,"Toulouse":35,"Guingamp":26,
+"Montpellier":42,"Lorient":36,"Nantes":40,"Caen":29,"Angers":25,
+"Bastia":28,"Metz":30,"Dijon":26,"Nancy":30}
+
+dic_2018={"Paris SG":540, "Monaco":180, "Lyon":240, "Marseille":170, "St Etienne":68,
+"Rennes":50, "Lille":90, "Bordeaux":70, "Nice":45, "Toulouse":34, "Guingamp":26,
+"Montpellier":43, "Nantes":45, "Caen":32, "Angers":28,
+"Metz":33, "Dijon":32,"Amiens":25,"Strasbourg":30,"Troyes":26}
+
+dic_2019={"Paris SG" : 500, 
+"Monaco" : 200,
+"Lyon" : 285,
+"Marseille" : 160,
+"St Etienne" : 74, 
+"Lille" : 90,
+"Rennes" : 68,
+"Montpellier" : 41.4,
+"Nice" : 50,
+"Nantes"  : 60,
+"Bordeaux" : 70,
+"Strasbourg"  :37.5,
+"Reims" : 40,
+"Angers": 30,
+"Toulouse"  : 35,
+"Nimes"  :20,
+"Guingamp" : 30,
+"Amiens"  :36,
+"Dijon"  : 35,
+"Caen" :34}
+
+dic_2020={"Paris SG" : 580,
+"Lyon" : 310,
+"Marseille" : 110,
+"Monaco" :  220,
+"Lille" : 110,
+"Rennes" : 68,
+"Montpellier" : 40,
+"Bordeaux" : 65,
+"Nice" : 50,
+"St Etienne" : 100,
+"Strasbourg" : 43,
+"Nantes" : 70,
+"Angers" : 32,
+"Amiens" : 30,
+"Toulouse" : 35,
+"Reims" : 45,
+"Metz" : 40,
+"Brest" : 30,
+"Dijon" : 38,
+"Nimes" : 27}
+
+dic_2021={"Paris SG" : 600,
+"Marseille" : 140,
+"Monaco" : 215,
+"Lyon" : 285,
+"Lille" : 147,
+"Rennes" : 105,
+"Nice" : 80,
+"Montpellier" : 54.5,
+"Strasbourg" :50,
+"Angers" : 45,
+"Bordeaux" : 50 ,
+"Nantes" : 75,
+"St Etienne" : 95,
+"Reims" : 70,
+"Lens" : 46,
+"Nimes" : 40,
+"Metz" : 50,
+"Brest" : 35,
+"Dijon" : 50,
+"Lorient" : 45}
+
+Budget = {
+    "2015_2016":dic_2016,
+    "2016_2017":dic_2017,
+    "2017_2018":dic_2018,
+    "2018_2019":dic_2019,
+    "2019_2020":dic_2020,
+    "2020_2021":dic_2021
+}
+
+
